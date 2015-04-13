7b61ff1b82ace50060798de19314ad4bc76b0468
BuildTools <jessebannon@live.com>
2015-04-13 12:09:05 -0700

    alt/base state toggle 30x30x8 regions

--- care_package/src/main/java/com/projectzombie/care_package/AltState.java
+++ care_package/src/main/java/com/projectzombie/care_package/AltState.java
@@ -5,11 +5,10 @@
  */
 package com.projectzombie.care_package;
 
-import java.lang.reflect.Array;
-import java.util.LinkedList;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.util.Vector;
 
@@ -20,45 +19,123 @@ import org.bukkit.util.Vector;
 public class AltState {
 
     private class BlockInfo {
-	private Material material;
-	private Byte data;
+	private final Material material;
+	private final Byte data;
 	
-	private BlockInfo(Material material, 
-		          Byte data) 
+	private BlockInfo(final Material material, 
+		          final Byte data) 
 	{
 	    this.material = material;
 	    this.data = data;
 	}
     }
     
-    private Location NW_location;
-    private BlockInfo[][][] AltStateBlocks = new BlockInfo[30][30][8];
+    private Location altStateLoc;
+    private Location baseStateLoc;
     
-    public AltState(final String altStateLocation, 
+    private final String altStateName;
+    private final String baseStateName;
+    
+    private String altStateDesc;
+    private String baseStateDesc;
+    
+    private BlockInfo[][][] blockBuffer = new BlockInfo[30][30][8];
+    
+    private boolean success;
+    
+    public AltState(final String altStateName,
+                    final String baseStateName,
 	            final FileConfiguration file) 
     {
-	final String path = "alt_state_locs." + altStateLocation;
-	final String worldPath = path + ".world";
-	final String coordPath = path + ".coords";
-	
-	if (file.contains(path) 
-		&& file.contains(worldPath) 
-		&& file.contains(coordPath))
+        /* Initialize file paths */
+        this.altStateName = altStateName;
+	final String altPath = "alt_states." + this.altStateName;
+	final String altWorldPath = altPath + ".world";
+	final String altCoordPath = altPath + ".coords";
+        final String altDescPath = altPath + ".desc";
+        
+        this.baseStateName = baseStateName;
+	final String basePath = "base_states." + this.baseStateName;
+	final String baseWorldPath = basePath + ".world";
+	final String baseCoordPath = basePath + ".coords";
+        final String baseDescPath = basePath + ".desc";
+        
+	if (!file.contains(altPath) 
+		|| !file.contains(altWorldPath) 
+		|| !file.contains(altCoordPath)
+                || !file.contains(altDescPath)
+                || !file.contains(basePath) 
+		|| !file.contains(baseWorldPath) 
+		|| !file.contains(baseCoordPath)
+                || !file.contains(baseDescPath))
 	{
-	    final Vector vector = file.getVector(coordPath);
-	    NW_location = new Location(
-		    Bukkit.getWorld(file.getString(worldPath)),
-		    vector.getX(), 
-		    vector.getY(), 
-		    vector.getZ());
+            return;
+        } 
+        
+        altStateDesc = file.getString(altDescPath);
+        baseStateDesc = file.getString(baseDescPath);
+        
+        final Vector altVector = file.getVector(altCoordPath);
+        final Vector baseVector = file.getVector(baseCoordPath);
+        
+        altStateLoc = new Location(
+                Bukkit.getWorld(file.getString(altWorldPath)),
+                altVector.getX(), 
+                altVector.getY(), 
+                altVector.getZ());
+        
+        baseStateLoc = new Location(
+                Bukkit.getWorld(file.getString(baseWorldPath)),
+                baseVector.getX(), 
+                baseVector.getY(), 
+                baseVector.getZ());
 	    
-	    for (int i = 0; i < 30; i++) {
-		for (int j = 0; j > -30; j--) {
-		    for (int k = 0; k < 8; k++) {
-			
-		    }
-		}
-	    }
-	}
+        final Block altInitBlock = altStateLoc.getBlock();
+        final Block baseInitBlock = baseStateLoc.getBlock(); 
+            
+        for (int i = 0; i < 30; i++) 
+        {
+            for (int j = 0; j < 30; j++) 
+            {
+                for (int k = 0; k < 8; k++) 
+                {
+                    blockBuffer[i][j][k] = new BlockInfo(
+                        baseInitBlock.getRelative(i, k, j).getType(),
+                        baseInitBlock.getRelative(i, k, j).getData());
+                    
+                    baseInitBlock.getRelative(i, k, j).setType(
+                        altInitBlock.getRelative(i, k, j).getType());
+                    
+                    baseInitBlock.getRelative(i, k, j).setData(
+                        altInitBlock.getRelative(i, k, j).getData());
+                }
+            }
+        }	
+    }
+    
+    public void restoreState() 
+    {
+        if (blockBuffer == null)
+            return;
+        
+        final Block baseInitBlock = baseStateLoc.getBlock(); 
+        
+        for (int i = 0; i < 30; i++)
+        {
+            for (int j = 0; j < 30; j++) 
+            {
+                for (int k = 0; k < 8; k++) 
+                {
+                    baseInitBlock.getRelative(i, j, k).setType(
+                        blockBuffer[i][j][k].material);
+                    
+                    baseInitBlock.getRelative(i, j, k).setData(
+                        blockBuffer[i][j][k].data);
+                    
+                    blockBuffer[i][j][k] = null;
+                }
+            }
+        }
+        blockBuffer = null;
     }
 }
