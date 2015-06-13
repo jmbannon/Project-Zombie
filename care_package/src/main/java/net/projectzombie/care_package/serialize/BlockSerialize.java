f08ba283054d460bb061e2735e994aaf95b5c298
BuildTools <jessebannon@live.com>
2015-06-12 23:45:59 -0700

    Changed project name

--- care_package/src/main/java/net/projectzombie/care_package/serialize/BlockSerialize.java
+++ care_package/src/main/java/net/projectzombie/care_package/serialize/BlockSerialize.java
@@ -24,8 +24,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.Server;
 import org.bukkit.block.Block;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 
