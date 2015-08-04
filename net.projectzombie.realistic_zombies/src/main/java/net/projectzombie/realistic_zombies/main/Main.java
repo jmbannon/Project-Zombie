66df6dba3d0b3c7e91b8371abe3ce2833d6f64ad
jmbannon <jmbannon@uw.edu>
2015-08-03 22:42:19 -0700

    initialized

---
+++ net.projectzombie.realistic_zombies/src/main/java/net/projectzombie/realistic_zombies/main/Main.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.projectzombie.realistic_zombies.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author jesse
+ */
+public class Main extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+//        this.getDataFolder().mkdir();
+//        Utilities.initialize(this);
+//        commandExecutor = new CommandExec();
+//        blockListener = new BlockListener();
+//        toolListener = new ToolListener();
+//        
+//        this.getCommand("bp").setExecutor(commandExecutor);
+//        this.getCommand("engrave").setExecutor(commandExecutor);
+//        this.getServer().getPluginManager().registerEvents(blockListener, this);
+//        this.getServer().getPluginManager().registerEvents(toolListener, this);
+//        this.getLogger().info("Placable Blocks enabled!");
+    }
+
+    @Override
+    public void onDisable()
+    {
+        this.getLogger().info("Realistic Zombies disabled!");
+    }
+}
